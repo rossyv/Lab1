df9bc0564ee0ee2b3b36a85d5f1c1f2218c9bc74
rosa maria vega jinez <rossy.vega14@hotmail.com>
2020-03-02 12:01:21 -0500

    se agrego controlador opciones

---
+++ Controllers/OPCIONESController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPlantilla.Controllers
+{
+    public class OPCIONESController : Controller
+    {
+        //
+        // GET: /OPCIONES/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
